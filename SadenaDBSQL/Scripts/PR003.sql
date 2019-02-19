4e666960298de6f056f474742246d0d67603b507
Jalboros@ <jalboros@gmail.com>
2019-02-19 01:39:05 -0600

    Se agregan procedimientos de oficialias

--- SadenaDBSQL/Scripts/PR003.sql
+++ SadenaDBSQL/Scripts/PR003.sql
@@ -40,7 +40,57 @@ BEGIN TRY
 
 END TRY
 BEGIN CATCH
-		SELECT @po_msg_code=-1, @po_msg = 'Error al obtener subregistro ' + ERROR_MESSAGE()
+		SELECT @po_msg_code=-1, @po_msg = 'Error al obtener las oficialias ' + ERROR_MESSAGE()
+		GOTO ERROR
+END CATCH
+	
+SET NOCOUNT OFF
+RETURN 0        
+       
+ERROR:        
+	RAISERROR (@po_msg,18,1)      
+	SET NOCOUNT OFF        
+	RETURN -1      
+ GO
+
+ IF EXISTS (SELECT name FROM SysObjects WITH ( NOLOCK ) WHERE ID = OBJECT_ID('SDB.PRSOficialia') AND SysStat & 0xf = 4)
+BEGIN
+	DROP PROC SDB.PRSOficialia
+END
+GO
+----------------------------------------------------------------------------------------------------------------------------------      
+--- Responsable: Jorge Alberto de la Rosa  
+--- Fecha      : Diciembre 2018  
+--- Descripcion: Creación de un stored procedure que consulta una oficialia
+--- Aplicacion:  SADENADB  
+----------------------------------------------------------------------------------------------------------------------------------  
+CREATE PROCEDURE SDB.PRSOficialia(
+@pi_oid int,
+@po_msg_code INT OUTPUT,
+@po_msg	VARCHAR(255) OUTPUT)
+
+AS 	
+	
+	SET NOCOUNT ON
+
+BEGIN TRY		
+
+	SELECT o.fi_oid AS OId,o.fi_oficialia_id AS OficialiaId,
+	o.fi_mpio_id AS MpioId, l.fc_loc_mpio_desc AS MpioDesc, o.fi_loc_id AS LocId, l.fc_loc_desc AS LocDesc,
+	o.fc_calle AS Calle,o.fc_numero AS Numero,o.fc_colonia AS Colonia,o.fc_cp AS CP,
+	o.fc_telefono AS Telefono, o.fc_nombres AS Nombres, o.fc_apellidos AS Apellidos, 
+	o.fc_correo_e AS CorreoE, o.fc_latitud AS Latitud, o.fc_longitud AS Longitud, 
+	o.fc_observaciones AS Observaciones 
+	FROM SDB.TAOficialias o
+	INNER JOIN SDB.CTLocalidad l
+	ON o.fi_edo_id = l.fi_loc_edo_id AND o.fi_mpio_id = L.fi_loc_mpio_id AND o.fi_loc_id = l.fi_loc_id
+	WHERE o.fi_oid = @pi_oid
+	
+	SELECT @po_msg_code=0, @po_msg = 'La ejecución del procedimiento fue exitosa'	
+
+END TRY
+BEGIN CATCH
+		SELECT @po_msg_code=-1, @po_msg = 'Error al obtener la oficialia ' + ERROR_MESSAGE()
 		GOTO ERROR
 END CATCH
 	
@@ -89,6 +139,12 @@ DECLARE
 @viConsecutivo INT
 	
 SET NOCOUNT ON
+
+IF EXISTS(SELECT 1 FROM SDB.TAOFICIALIAS WHERE fi_oficialia_id = @pi_oficialia_id)
+BEGIN
+	SELECT @po_msg_code=-1, @po_msg = 'El número de Oficialía ya existe, favor de validar la información'
+	GOTO ERROR
+END
 
 BEGIN TRY		
 
